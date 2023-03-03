104bdce7ae6796877146d9906d6894008ff9992d
Dylan <rakt43dylan53@gmail.com>
2023-03-03 18:54:43 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 const result = document.getElementById('result');
-const buttons = document.querySelectorAll('.buttons button');
+const buttons = document.querySelectorAll('.calculator-buttons button');
+var operatorUsed = false;
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         if (button.innerHTML === '=') {
             result.value = eval(result.value);
-        } else if (button.innerHTML === 'C') {
+            operatorUsed = false; // Reset operatorUsed after evaluation
+        } 
+        else if (button.innerHTML === 'C') {
             result.value = '';
-        } else {
+            operatorUsed = false; // Reset operatorUsed after clear
+        }
+        else if (["+","-","*","/"].indexOf(button.innerHTML) > -1){
+            if (!operatorUsed){
+                result.value += button.innerHTML;
+                operatorUsed = true;
+            }
+            else {
+                result.value = result.value.slice(0, -1) + button.innerHTML; // Replace last operator used
+            }
+        }
+        else {
             result.value += button.innerHTML;
+            operatorUsed = false; // Reset operatorUsed after number key press
         }
     });
 });
