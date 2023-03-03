5483d94cf940d4f9cdb5a06ded2b62ace4637776
Dylan <rakt43dylan53@gmail.com>
2023-03-03 19:38:35 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
 const result = document.getElementById('result');
 const buttons = document.querySelectorAll('.calculator-buttons button');
-var operatorUsed = false;
+
+// Create spark element and append to body
+const spark = document.createElement('div');
+spark.className = 'spark';
+document.body.appendChild(spark);
 
 buttons.forEach(button => {
+    const soundId = `button-sound-${button.innerText.toLowerCase()}`;
+    const audioElement = document.getElementById(soundId);
+    
     button.addEventListener('click', () => {
         if (button.innerHTML === '=') {
             result.value = eval(result.value);
-            operatorUsed = false; // Reset operatorUsed after evaluation
+            document.getElementById('button-sound-equals').currentTime = 0;
+            document.getElementById('button-sound-equals').play(); // Play button sound for =
         } 
         else if (button.innerHTML === 'C') {
             result.value = '';
-            operatorUsed = false; // Reset operatorUsed after clear
-        }
-        else if (["+","-","*","/"].indexOf(button.innerHTML) > -1){
-            if (!operatorUsed){
-                result.value += button.innerHTML;
-                operatorUsed = true;
-            }
-            else {
-                result.value = result.value.slice(0, -1) + button.innerHTML; // Replace last operator used
-            }
+            document.getElementById('button-sound-clear').currentTime = 0;
+            document.getElementById('button-sound-clear').play(); // Play button sound for C
         }
         else {
             result.value += button.innerHTML;
-            operatorUsed = false; // Reset operatorUsed after number key press
+            if (/\+|\-|\*|\//.test(button.innerHTML)) { // test if the button is a symbol (+,-,*,/)
+                document.getElementById('button-sound-symbol').currentTime = 0;
+                document.getElementById('button-sound-symbol').play(); // Play button sound for symbols
+            } else {
+                audioElement.currentTime = 0;
+                audioElement.play(); // Play button sound
+            }
         }
+
+        // Animate spark effect
+        const rect = button.getBoundingClientRect();
+        const x = rect.left + rect.width / 2;
+        const y = rect.top + rect.height / 2;
+        spark.style.left = `${x}px`;
+        spark.style.top = `${y}px`;
+        spark.classList.add('animate');
+
+        // Remove animation class after 500 milliseconds
+        setTimeout(() => {
+            spark.classList.remove('animate');
+        }, 500);
     });
 });
